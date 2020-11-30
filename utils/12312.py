2c48603ca1e056c09cce4e6e830f09a4ebce9a34
张浩 <769314439@qq.com>
2020-11-30 23:01:17 +0800

    2020113002

--- utils/12312.py
+++ utils/12312.py
@@ -3,7 +3,7 @@
 ================================================================================
 
 Author : Administrator
-Created  on : 2020/11/29
+Created  on : 2020/11/30
 
 E-mail: zh13997821732@163.com
 
@@ -11,19 +11,4 @@ E-mail: zh13997821732@163.com
 ================================================================================
 
 """
-import os
-
-from common.constant import DATA_DIR
-from common.config import conf
-file_path = os.path.join(DATA_DIR, 'api_automation.course.xlsx')
-print(file_path)
-
-url = conf.get('env', 'url') + '/user/login/'
-print(url)
-
-
-
-
-
-
-
+# 2132131
